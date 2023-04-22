d93f8114f0ff6f3b07499005cb4d017e2e9cb407
ashat2005 <ashatkydyrov61@gmail.com>
2023-04-22 17:25:20 -0400

    dz7

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,6 @@
-urlpatterns = [
+from django.urls import path
+from users.views import RegisterView
 
+urlpatterns = [
+    path("users/register/", RegisterView.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from users.forms import UserRegistrationForm
 
-# Create your views here.
+class RegisterView(FormView):
+    form_class = UserRegistrationForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("register_done")
+
+    def form_valid(self, form):
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data.get("password2"))
+        new_user.save()
+        return super().form_valid(form)
