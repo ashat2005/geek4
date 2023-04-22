138d99a45770c361cbfacff7413ff7c96ace2b9e
ashat2005 <ashatkydyrov61@gmail.com>
2023-04-22 18:17:06 -0400

    dz7

--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.shortcuts import render
+from django.views import View
 from users.forms import UserRegistrationForm
 
-class RegisterView(FormView):
-    form_class = UserRegistrationForm
-    template_name = "registration/register.html"
-    success_url = reverse_lazy("register_done")
 
-    def form_valid(self, form):
-        new_user = form.save(commit=False)
-        new_user.set_password(form.cleaned_data.get("password2"))
-        new_user.save()
-        return super().form_valid(form)
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, "registration/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password2'])
+            new_user.save()
+            return render(request, "registration/register_done.html", {"user": new_user})
+        return render(request, "registration/register.html", {"form": form})
+
+
